d48695ef37ad1738f65154b35a13ce13bcfee3c8
Matt Sullivan <mattsully01@gmail.com>
2025-06-03 11:58:45 -0600

    Updated requirements.txt

--- test_token_gen.py
+++ test_token_gen.py
@@ -1,5 +1,5 @@
 from utils.token_utils import generate_signed_feedback_url
 
 # Pick an encounter ID you know exists in your DB
-url = generate_signed_feedback_url("e1001")
+url = generate_signed_feedback_url("e1007")
 print("Test feedback link:", url)
